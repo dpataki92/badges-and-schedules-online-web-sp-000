53207ad9d78024ce4cdebba3a38fdbd2f1569536
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-03-20 21:36:44 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,34 @@
 # Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+
+  messages_array = []
+
+  names.each do |el|
+    messages_array << badge_maker(el)
+  end
+
+  messages_array
+
+end
+
+def assign_rooms(names)
+
+  room_num_arr = []
+
+  names.each_with_index {|name, i| room_num_arr << [name, i]}
+
+  room_num_arr.collect {|name_and_room| "Hello, #{name_and_room[0]}! You'll be assigned to room #{name_and_room[1] + 1}!"}
+
+end
+
+def printer(names)
+  7.times do {
+    puts batch_badge_creator(names)
+    puts assign_rooms(names)
+  }
+end
